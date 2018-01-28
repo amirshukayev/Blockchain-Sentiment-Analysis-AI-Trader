c270650b3abf66130171bafe2fd1911d041a5294
amirshukayev <amir.shukayev@gmail.com>
2018-01-28 07:05:59 -0700

    promises + .then() finally working

--- update.js
+++ update.js
@@ -19,26 +19,67 @@ var number = 0;
 // RUN WITH flask run --host=0.0.0.0
 function updateit() {
 
-	setTimeout(function(){
-		$.ajax({
-	           type: "GET",
-	           url: "http://0.0.0.0:5000/getstuff",
-	           success: function (data, textStatus, XMLHttpRequest) {
-	               alert(data);
-	               alert("success!");
-	           },
-	           error: function(XMLHttpRequest, textStatus, errorThrown) {
-	               alert("fail");
-	           }
-	     });
+	whattt().then(function(data){
+		data = JSON.parse(data);
+		console.log("does this work?");
+
+		for (var key in data) {
+		    if (data.hasOwnProperty(key)) {
+		        if (data[key] > 0.05){
+		        	
+		        }
+		    }
 		}
-	,30000);
+		
+		/*
+		var element = document.getElementById("click");
+		element.innerHTML = "the number is: " + number
+		*/
+	});
 
+	/*
+	var Data;
 
-	var element = document.getElementById("click");
-	element.innerHTML = "the number is: " + number
-	number++;
+	var promise1 = new Promise(function(resolve,reject){
+		setTimeout(function(){
+			$.ajax({
+		           type: "GET",
+		           url: "http://0.0.0.0:5000/getstuff",
+		           success: function (data, textStatus, XMLHttpRequest) {
+		               alert(data);
+		               Data = data
+		           },
+		           error: function(XMLHttpRequest, textStatus, errorThrown) {
+		               alert("fail");
+		           }
+		     });
+			}
+		,30000);
 
+		if (Data != undefined){
+			resolve(Data);
+		}
+		else {
+			reject();
+		}
+	});
+
+	promise1.then(function(fromResolve){
+		console.log(fromResolve)
+		var element = document.getElementById("click");
+		element.innerHTML = "the number is: " + number
+		number++;
+	}).catch(function(fromReject){
+		console.log("shit is not working");
+	})
+	*/
 }
 
-postData('data to process');
+const whattt = async() => {
+	return await fetch("http://0.0.0.0:5000/getstuff")
+		.then(res => res.json())
+		.then(data =>{
+			console.log(data);
+			return data;
+		})
+}
