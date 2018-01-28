c323670c6f8d70c4f00228211608d76aa87ba507
amirshukayev <amir.shukayev@gmail.com>
2018-01-28 05:34:41 -0700

    fixed

--- flaskServer.py
+++ flaskServer.py
@@ -1,11 +1,12 @@
 import flask
 import scraper
+from flask_cors import CORS
 
 app = flask.Flask(__name__)
-
+CORS(app)
 
 @app.route('/getstuff')
 def update_sentiments():
     #return scraper.startUp()
-    return scraper.startUp()
+    return flask.jsonify(scraper.startUp())
 
